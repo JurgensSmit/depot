63a1f67a963f57f53100d004ccf88f7216a644e9
Jurgens Smit <contact@jurgenssmit.com>
2013-04-24 12:53:20 +1000

    loaded store and product pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Depot::Application.routes.draw do
+  get "store/index"
+
+  resources :products
+
+
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
+  root :to => "store#index", :as => "store"
   devise_for :users
   resources :users
 end
